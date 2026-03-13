39e63a02b6d6476bde3e0349a6257db863690cf6
라경연 <kyrah@rsupport.com>
2026-03-13 14:18:40 +0900

    Configure Supabase connection

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://rrdvreopvvvnbhvnstzg.supabase.co",
+  supabaseAnonKey: "sb_publishable_NSuKCEuycHiSwPL1l7zdKQ_I85-PQGB",
   supabaseTable: "team_scheduler_state",
 };
